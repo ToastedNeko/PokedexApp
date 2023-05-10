0ac5f528ed52c4f196492af431b73dc38c8259ae
Tori Bialorucki <60600706+ToastedNeko@users.noreply.github.com>
2023-05-09 22:58:20 -0700

    Pokeball changed for opacity, added button to click whether Pokemon is owned
    Pokemon owned not working properly and needs to be fixed

--- Pokedex/Views/PokemonDetailView.swift
+++ Pokedex/Views/PokemonDetailView.swift
@@ -15,6 +15,9 @@ struct PokemonDetailView: View {
     @State var pokemonSelected = PokemonSelected(sprites: PokemonSprites(), name: "", weight: 0, height: 0, species: Species(), types: [PokemonType]())
     @State var pokemonSpecies = PokemonSpecies(flavor_text_entries: [FlavorTextEntry]())
     @State var flavorText = ""
+    @State var showPokeBall = "GrayPokeball3"
+//    @State var showRedPokeBall = false
+//    @State var showGrayPokeBall = true
     
     var body: some View {
         ScrollView{
@@ -40,11 +43,11 @@ struct PokemonDetailView: View {
                     Spacer().frame(height: 10)
                     
                     VStack(spacing: 6){
-
+                        
                         Text(pokemonEntry.name.capitalized)
                         Text("Height: " + String(round(Double(pokemonSelected.height) * 3.93701 )) + "\"")
                         Text("Weight: " + String(round(Double(pokemonSelected.weight) / 4.536)) + " lbs")
-                       
+                        
                     }.fixedSize(horizontal: false, vertical: true).padding(EdgeInsets(top: 1, leading: 1, bottom: 1, trailing: 1)).multilineTextAlignment(.leading).background(Color.white).cornerRadius(6)
                     Spacer().frame(height: 5)
                 }
@@ -54,7 +57,7 @@ struct PokemonDetailView: View {
                     RoundedRectangle(cornerRadius: 10)
                         .stroke(Color.black, lineWidth: 5)
                 )
-               
+                
                 VStack{
                     
                     
@@ -67,31 +70,60 @@ struct PokemonDetailView: View {
                 }
                 
                 VStack(alignment: .leading, spacing: 6){
-
-                                    HStack{
-                                        Text(self.flavorText).fixedSize(horizontal: false, vertical: true).padding(EdgeInsets(top: 1, leading: 1, bottom: 1, trailing: 1)).multilineTextAlignment(.leading)
-                                        
-                                    }.cornerRadius(6).padding(EdgeInsets(top: 1, leading: 1, bottom: 1, trailing: 1)).background(Color.white)
-                                        .font(.custom("GillSans", size: 25))
-
+                    
+                    HStack{
+                        Text(self.flavorText).fixedSize(horizontal: false, vertical: true).padding(EdgeInsets(top: 1, leading: 1, bottom: 1, trailing: 1)).multilineTextAlignment(.leading)
+                        
+                    }.cornerRadius(6).padding(EdgeInsets(top: 1, leading: 1, bottom: 1, trailing: 1)).background(Color.white)
+                        .font(.custom("GillSans", size: 25))
+                    
                 }.cornerRadius(10).padding(EdgeInsets(top: 10, leading: 80, bottom: 10, trailing: 80)).background(.yellow).shadow(color: .black.opacity(0.4), radius: 5, x: 8, y: 8).overlay(
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .stroke(Color.black, lineWidth: 5)
-                                )
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.black, lineWidth: 5)
+                )
                 Spacer().frame(height: 5)
                 
-                HStack{
-                    //Text("Owned: ")
-                    Button("Owned"){
-                        // show a pokeball
-                    }.background(Color.red).scenePadding()
-                    Button("Not Owned"){
-                        // show a grayed out, low opacity pokeball
-                    }.background(Color.red).scenePadding()
+                VStack{
+                    
+                    Image(showPokeBall)
+                        .aspectRatio(contentMode: .fit)
+                    
+                    Button(action: {
+                        self.showPokeBall = "Pokeball3"
+                    }, label: {
+                        Text("Owned")
+                    })
+                    
+//                    Button("Owned"){
+//                        // show a pokeball
+//                        showPokeBall.toggle()
+//                        //showRedPokeBall.disabled(false)
+//
+//
+//                    }.background(Color.red)
                     
                     
-                }.padding(EdgeInsets(top: 50, leading: 0, bottom: 10, trailing: 0))
+//                    Button("Not Owned"){
+//                        // show a grayed out, low opacity pokeball
+//                        showGrayPokeBall.toggle()
+//                        //showRedPokeBall.disabled(false)
+//
+//                    }.background(Color.red).scenePadding()
+                    
+                    // Pokeball3
+                    // GrayPokeball3
+//
+//                    if(showRedPokeBall){
+//
+//                    }
+                    
+//                    if(showGrayPokeBall){
+//                        Image("GrayPokeball3")
+//                        Image(self.showPokeBall).hidden()
+//                    }
 
+                }.padding(EdgeInsets(top: 50, leading: 0, bottom: 10, trailing: 0))
+                
             }.background(Image("Grid").opacity(0.10))
             
         }
@@ -105,7 +137,7 @@ struct PokemonDetailView: View {
             
             self.pokemonSelected = data
             let speciesUrl = data.species.url
-
+            
             PokemonSpeciesApi().getData(url: speciesUrl){ speciesData in
                 self.pokemonSpecies = speciesData
                 self.flavorText = speciesData.flavor_text_entries[0].flavor_text
